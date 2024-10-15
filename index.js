00efbf3668d21be01de09b9fc94bcc9f3e4717c1
soodala2003 <soodala78@gmail.com>
2024-10-15 10:32:03 -0400

    added part2 in index.js file

---
+++ index.js
@@ -0,0 +1,44 @@
+// Part 1: Humble Beginnings
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat",
+        companion: {
+            name: "Frank",
+            type: "Flea",
+            belongings: ["small hat", "sunglasses"]
+        }
+    },
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`);
+    }
+}
+
+// Part 2: Class Fantasy
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`);
+    }
+}
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+
+  
+    
